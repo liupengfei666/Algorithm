08e73896b0d37a9708147b408945a7e28ffad369
liupengfei <liupengfei@kingsoft.com>
2020-10-29 14:32:16 +0800

    异或运算练习

---
+++ src/com/lpf/basic/class01/Code08_EvenTimesOddTimes.java
@@ -0,0 +1,60 @@
+package com.lpf.basic.class01;
+
+public class Code08_EvenTimesOddTimes {
+
+    //arr中，只有一种数出现了奇数次
+    public static void printOddTimesNum1(int[] arr) {
+        int eor = 0;
+        for (int i = 0; i < arr.length; i++) {
+            eor ^= arr[i];
+        }
+        System.out.println(eor);
+    }
+
+    //arr中，有两种数出现了奇数次
+    public static void printOddTimesNum2(int[] arr) {
+        int eor = 0;
+        for (int i = 0; i < arr.length; i++) {
+            eor ^= arr[i];
+        }
+        int rightOne = eor & (~eor + 1); //提取出最右侧的1
+        int onlyOne = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if ((arr[i] & rightOne) != 0) {
+                onlyOne ^= arr[i];
+            }
+        }
+        System.out.println(onlyOne + " " + (eor ^ onlyOne));
+    }
+
+    //求二进制数1的个数
+    public static int bit1Counts(int N) {
+        int count = 0;
+        while (N != 0) {
+            int rightOne = N & (~N + 1);
+            count++;
+            N ^= rightOne;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+
+        System.out.println(a);
+        System.out.println(b);
+
+        int[] arr1 = { 3, 3, 2, 3, 1, 1, 1, 3, 1, 1, 1 };
+        printOddTimesNum1(arr1);
+
+        int[] arr2 = { 4, 3, 4, 2, 2, 2, 4, 1, 1, 1, 3, 3, 1, 1, 1, 4, 2, 2 };
+        printOddTimesNum2(arr2);
+
+        System.out.println(bit1Counts(5)); // 0101
+    }
+}
